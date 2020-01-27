9077e072ec0bda53c27c2174d5e1a9e46c3f5d89
Babelbloc <bablebloc@gmail.com>
2020-01-27 16:29:57 +0100

    alexis

--- app/src/main/java/net/lemarechal/corentin/babelbloc/MainActivity.java
+++ app/src/main/java/net/lemarechal/corentin/babelbloc/MainActivity.java
@@ -21,6 +21,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -52,151 +53,151 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-    try {
-    sqLite =new SQLite(getApplicationContext(),null,null,1);
-
-    alertB = new AlertDialog.Builder(this);
-
-    final Button[] ajouter = new Button[1];
-    listeView = findViewById(R.id.lsView);
-    lsTest = findViewById(R.id.lstest);
-    int layoutID = R.layout.item_perso;
-    listeView.setAdapter(arrayPersonnelAdapter);
-
-        if(savedInstanceState != null){
-            arrayPersonnel = ((ArrayList<Personnel>) savedInstanceState.getSerializable(MainActivity.TABLE_P_KEY));
-        }
-
-    arrayPersonnelAdapter = new ArrayPersonnelAdapter(this,layoutID,arrayPersonnel);
-    listeView.setAdapter(arrayPersonnelAdapter);
-    final ArrayAdapter<String> arrayListAdapter=   new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
-    lsTest.setAdapter(arrayListAdapter);
-
-    final EditText etPass= findViewById(R.id.Et_Pass);
-    final boolean[] conn = {false};
-
-       AfficherListe();
-
-       new Thread(new Runnable() {
-           @Override
-           public void run() {
-               try{
-                   Excel excel = new Excel();
-                   strings= excel.LireExcel();
-                   runOnUiThread(new Runnable() {
-                       @Override
-                       public void run() {
-                           do{
-                               if(strings.get(i).equals("Text")) {
-                                   retour = sqLite.AjouterQuestion(strings.get(i+1),strings.get(i+2));
-                               }
-                               if(strings.get(i).equals("TextQCU"))
-                               if(retour <0){
-                                   runOnUiThread(new Runnable() {
-                                       @Override
-                                       public void run() {
-                                           try{
-
-                                           alertB.setTitle("Question");
-                                           switch (retour) {
-                                               case -3:
-                                                   alertB.setMessage("Identifiant déjà saisi").show();
-                                                   break;
-
-                                               case -2:
-                                                   alertB.setMessage("Problème lors de la création du compte").show();
-                                                   break;
-                                               case -1:
-                                                   alertB.setMessage("Compte déjà crée").show();
-                                                   break;
-                                               case 1:
-                                                   alertB.setMessage("Creation du compte réussi").show();
-                                           }
-
-                                       } catch (Exception e) {
-                                           alertB.setTitle(R.string.TitreErreur);
-                                           alertB.setMessage(e.getMessage());
-                                       }
-
-                                   }
-                                   });
-                               }
-                               arrayList.add(strings.get(i));
-                               arrayListAdapter.notifyDataSetChanged();
-                               i++;
-                           }
-                           while (strings.size()> i);
-                           alertB.setMessage(String.valueOf(arrayList.size())).show();
-                       }
-                   });
-               }
-               catch (Exception e){
-                   alertB.setMessage(e.getMessage()).show();
-               }
-           }
-       }).start();
-
-
-        listeView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-                try{
-                    final AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
-                    alert.setTitle(getResources().getString(R.string.dialConnTitre));
-                    alert.setMessage(getResources().getString(R.string.dialConnMessage));
-                    final Personnel p = arrayPersonnel.get(position);
-                    alert.setPositiveButton(getResources().getString(R.string.dialConnectOUI), new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            etPass.setEnabled(true);
-                            new Thread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    conn[0] =sqLite.Connection(p.PersonnelIdentifiant,etPass.getText().toString());
-                                    runOnUiThread(new Runnable() {
-                                        @Override
-                                        public void run() {
-
-                                            if(conn[0]){
-                                                alertB.setTitle(getResources().getString(R.string.dialConnTitre));
-                                                alertB.setMessage((R.string.dialidentenfiant_OK)).show();
-                                                connexion=true;
-                                                invalidateOptionsMenu();
-
-                                            }
-                                            else{
-                                                alertB.setMessage((R.string.dialidentifiant_KO)).show();
-                                            }
-                                        }
-                                    });
-                                }
-                            }).start();
-                        }
-                    });
-                    alert.setNegativeButton(getResources().getString(R.string.dialConnectNON), new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                        }
-                    });
-                    alert.show();
-                }
-                catch (Exception e){
-                    alertB.setTitle(R.string.TitreErreur);
-                    alertB.setMessage(e.getMessage());
-                }
-
+        try {
+            sqLite =new SQLite(getApplicationContext(),null,null,1);
+            if (sqLite.getDrapeaux().isEmpty()) {
+                //File f = new File("/storage/D2C3-7B63/drapeaux/");
+                File f = new File("/storage/emulated/legacy/Drapeaux/");
+                sqLite.reimportDrapeaux(f);
             }
-        });
+
+            alertB = new AlertDialog.Builder(this);
+
+            final Button[] ajouter = new Button[1];
+            listeView = findViewById(R.id.lsView);
+            lsTest = findViewById(R.id.lstest);
+            int layoutID = R.layout.item_perso;
+            listeView.setAdapter(arrayPersonnelAdapter);
+
+                if(savedInstanceState != null){
+                    arrayPersonnel = ((ArrayList<Personnel>) savedInstanceState.getSerializable(MainActivity.TABLE_P_KEY));
+                }
+
+            arrayPersonnelAdapter = new ArrayPersonnelAdapter(this,layoutID,arrayPersonnel);
+            listeView.setAdapter(arrayPersonnelAdapter);
+            final ArrayAdapter<String> arrayListAdapter=   new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
+            lsTest.setAdapter(arrayListAdapter);
+
+            final EditText etPass= findViewById(R.id.Et_Pass);
+            final boolean[] conn = {false};
+
+               AfficherListe();
+
+               new Thread(new Runnable() {
+                   @Override
+                   public void run() {
+                       try{
+                           Excel excel = new Excel();
+                           strings= excel.LireExcel();
+                           runOnUiThread(new Runnable() {
+                               @Override
+                               public void run() {
+                                   do{
+                                       if(strings.get(i).equals("Text")) {
+                                           retour = sqLite.AjouterQuestion(strings.get(i+1),strings.get(i+2));
+                                       }
+                                       if(strings.get(i).equals("TextQCU"))
+                                       if(retour <0){
+                                           runOnUiThread(new Runnable() {
+                                               @Override
+                                               public void run() {
+                                                   try{
+
+                                                   alertB.setTitle("Question");
+                                                   switch (retour) {
+                                                       case -3:
+                                                           alertB.setMessage("Identifiant déjà saisi").show();
+                                                           break;
+
+                                                       case -2:
+                                                           alertB.setMessage("Problème lors de la création du compte").show();
+                                                           break;
+                                                       case -1:
+                                                           alertB.setMessage("Compte déjà crée").show();
+                                                           break;
+                                                       case 1:
+                                                           alertB.setMessage("Creation du compte réussi").show();
+                                                   }
+
+                                               } catch (Exception e) {
+                                                   alertB.setTitle(R.string.TitreErreur);
+                                                   alertB.setMessage(e.getMessage());
+                                               }
+
+                                           }
+                                           });
+                                       }
+                                       arrayList.add(strings.get(i));
+                                       arrayListAdapter.notifyDataSetChanged();
+                                       i++;
+                                   }
+                                   while (strings.size()> i);
+                                   alertB.setMessage(String.valueOf(arrayList.size())).show();
+                               }
+                           });
+                       }
+                       catch (Exception e){
+                           alertB.setMessage(e.getMessage()).show();
+                       }
+                   }
+               }).start();
 
 
-}
-catch (Exception e){
-    alertB.setTitle(R.string.TitreErreur);
-    alertB.setMessage(e.getMessage()).show();
-}
+            listeView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                    try{
+                        final AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                        alert.setTitle(getResources().getString(R.string.dialConnTitre));
+                        alert.setMessage(getResources().getString(R.string.dialConnMessage));
+                        final Personnel p = arrayPersonnel.get(position);
+                        alert.setPositiveButton(getResources().getString(R.string.dialConnectOUI), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                etPass.setEnabled(true);
+                                new Thread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        conn[0] =sqLite.Connection(p.PersonnelIdentifiant,etPass.getText().toString());
+                                        runOnUiThread(new Runnable() {
+                                            @Override
+                                            public void run() {
 
+                                                if(conn[0]){
+                                                    alertB.setTitle(getResources().getString(R.string.dialConnTitre));
+                                                    alertB.setMessage((R.string.dialidentenfiant_OK)).show();
+                                                    connexion=true;
+                                                    invalidateOptionsMenu();
 
+                                                }
+                                                else{
+                                                    alertB.setMessage((R.string.dialidentifiant_KO)).show();
+                                                }
+                                            }
+                                        });
+                                    }
+                                }).start();
+                            }
+                        });
+                        alert.setNegativeButton(getResources().getString(R.string.dialConnectNON), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        });
+                        alert.show();
+                    }
+                    catch (Exception e){
+                        alertB.setTitle(R.string.TitreErreur);
+                        alertB.setMessage(e.getMessage());
+                    }
+
+                }
+            });
+        }
+        catch (Exception e){
+            alertB.setTitle(R.string.TitreErreur);
+            alertB.setMessage(e.getMessage()).show();
+        }
     }
     public void AfficherListe(){
         new Thread(new Runnable() {
